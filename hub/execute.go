aad981401bf8be5fc242e82f9e068e214de14cd0
Kalen Krempely <kkrempely@vmware.com>
2021-03-15 17:52:44 -0700

    execute: mark mirrors as down
    Do this before starting the target cluster to prevent the mirror
    processes from starting.
    
    This would not be needed if we did not initialize the target cluster
    with mirrors in link mode and instead "manually" add mirrors.

--- hub/execute.go
+++ hub/execute.go
@@ -4,6 +4,7 @@
 package hub
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -11,6 +12,7 @@ import (
 	"github.com/greenplum-db/gp-common-go-libs/gplog"
 	"golang.org/x/xerrors"
 
+	"github.com/greenplum-db/gpupgrade/db/connURI"
 	"github.com/greenplum-db/gpupgrade/idl"
 	"github.com/greenplum-db/gpupgrade/step"
 	"github.com/greenplum-db/gpupgrade/utils"
@@ -98,6 +100,57 @@ func (s *Server) Execute(request *idl.ExecuteRequest, stream idl.CliToHub_Execut
 			TablespacesMappingFile: s.TablespacesMappingFilePath,
 		})
 	})
+
+	// In link mode we don't want customers to touch the mirrors, so mark them
+	// down before starting the target cluster to prevent the mirror processes
+	// from running. This is needed since in link mode we chose to initialize
+	// the target cluster with mirrors rather than adding the mirrors "manually".
+	if s.UseLinkMode {
+		st.RunInternalSubstep(func() error {
+			return s.Target.StartMasterOnly(step.DevNullStream)
+		})
+
+		st.RunInternalSubstep(func() (err error) {
+			options := []connURI.Option{
+				connURI.ToTarget(),
+				connURI.Port(s.Target.MasterPort()),
+				connURI.UtilityMode(),
+				connURI.AllowSystemTableMods(),
+			}
+
+			uri := s.Connection.URI(options...)
+			connection, err := sql.Open("pgx", uri)
+			if err != nil {
+				return xerrors.Errorf("connecting to master on port %d in utility mode with connection URI '%s': %w", s.Target.MasterPort(), uri, err)
+			}
+
+			defer func() {
+				closeErr := connection.Close()
+				if closeErr != nil {
+					closeErr = xerrors.Errorf("closing connection to target master: %w", closeErr)
+					err = errorlist.Append(err, closeErr)
+				}
+			}()
+
+			// To ensure mirrors are properly recovered during finalize set the
+			// mode to not synchronized for both the mirrors and primaries.
+			_, err = connection.Exec(`UPDATE gp_segment_configuration SET status = 'd', mode = 'n' WHERE preferred_role = 'm' AND content <> '-1';`)
+			if err != nil {
+				return xerrors.Errorf("set mirror status to down: %w", err)
+			}
+
+			_, err = connection.Exec(`UPDATE gp_segment_configuration SET mode = 'n' WHERE preferred_role = 'p' AND content <> '-1';`)
+			if err != nil {
+				return xerrors.Errorf("set corresponding primary mode to change tracking: %w", err)
+			}
+
+			return nil
+		})
+
+		st.RunInternalSubstep(func() error {
+			return s.Target.StopMasterOnly(step.DevNullStream)
+		})
+	}
 
 	st.Run(idl.Substep_START_TARGET_CLUSTER, func(streams step.OutStreams) error {
 		err := s.Target.Start(streams)
